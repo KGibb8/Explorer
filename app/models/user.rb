1844a236b90f019116a678999f89d5183aa1e2a4
kgibb8 <kieran.a.gibb@gmail.com>
2016-12-05 10:10:31 +0000

    Add profile with user reference. Migrations for profile details.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_one :profile
+
   validates_presence_of :username
 end
