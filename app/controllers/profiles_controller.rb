bc9d49435b44d71542ea65d7011f638099dd24f8
kgibb8 <kieran.a.gibb@gmail.com>
2016-12-05 11:09:05 +0000

    Add update action for Profile. Revise JSON response test.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,23 @@
 class ProfilesController < ApplicationController
-  before_action :find_profile, only: [:show]
+  before_action :find_profile, only: [:show, :update]
 
   def show
 
+  end
+
+  def update
+    @profile.update(profile_params)
+    render json: @profile
   end
 
   private
 
   def find_profile
     @profile = User.find(params[:user_id]).profile
+  end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :biography, :avatar)
   end
 
 end
--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -22,4 +22,34 @@ RSpec.describe ProfilesController do
     end
   end
 
+  describe "update action for profile" do
+    before do
+      profile_picture = Rack::Test::UploadedFile.new(File.join(__dir__, "../assets", "shaka.jpg"))
+      params = {
+        user_id: shaka.id,
+        profile: {
+          first_name: "Shaka",
+          last_name: "Zulu",
+          biography: Faker::Lorem.paragraph,
+          avatar: profile_picture
+        }
+      }
+      @patch_update = Proc.new { patch :update, params: params, format: :json }
+    end
+
+    it "should return a 200 status" do
+      expect(response).to be_ok
+    end
+
+    it "should assign the profile in question" do
+      expect(@patch_update).to change { assigns :profile }
+    end
+
+    # it "should return a JSON response" do
+    #   process :update, method: :patch, params: params
+    #   expect(response.body).to eq shaka.profile.to_json
+    # end
+
+  end
+
 end
