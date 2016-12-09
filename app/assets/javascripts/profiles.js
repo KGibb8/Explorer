a72a1792412223bd6f72f15a80d755a86321beca
kgibb8 <kieran.a.gibb@gmail.com>
2016-12-09 17:16:27 +0000

    Friends paginated on Profile page using AJAX

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -1,50 +1,57 @@
+$(document).ready(function () {
 
-// Ajax responses
-$('#submitAvatar').on('ajax:remotipartComplete', function (e, data) {
-  var profile = JSON.parse(data.responseText);
-  $('.avatar')[0].style = "background-image: url(" + profile.avatar.profile.url + ")";
+  $('#submitAvatar').on('ajax:remotipartComplete', function (e, data) {
+    var profile = JSON.parse(data.responseText);
+    $('.avatar')[0].style = "background-image: url(" + profile.avatar.profile.url + ")";
+  });
+
+  $('#submitBiography').on('ajax:success', function (e, data) {
+    $('#biography > p').html(data.biography);
+  });
+
+  $($('#requestFriend').parent()[0]).on('ajax:success', function (e, data) {
+    var column = this.parentElement;
+    $(column).empty();
+    var ele = document.createElement('p');
+    ele.innerHTML = data.message;
+    column.appendChild(ele);
+  });
+
+  $($('#acceptFriend').parent()[0]).on('ajax:success', function (e, data) {
+    var container = this.parentElement;
+    $(container).empty();
+    var ele = document.createElement('p');
+    ele.innerHTML = data.message;
+    container.appendChild(ele);
+    // %%TODO%% Render partial instead!!
+    var friend = createFriend(data.user, data.profile);
+    $('#friends').append(friend);
+  });
+
+  $($('#rejectFriend').parent()[0]).on('ajax:success', function (e, data) {
+    var container = this.parentElement;
+    $(container).empty();
+    var ele = document.createElement('p');
+    ele.innerHTML = data.message;
+    container.appendChild(ele);
+  });
+
+  $('.friends_pagination a').on("click", function (){
+    $.get(this.href, null, null, 'script');
+    return false;
+  });
+
+  function createFriend (user, profile) {
+    var friend = document.createElement('div');
+    friend.className = 'friend'
+      var pic = document.createElement('div');
+    pic.className = "avatar thumb";
+    pic.style = "background-image: url(" + profile.avatar.thumb.url + ");";
+    friend.appendChild(pic);
+    var p = document.createElement('p');
+    p.innerHTML = user.username;
+    friend.appendChild(p);
+    return friend
+  }
 });
 
-$('#submitBiography').on('ajax:success', function (e, data) {
-  $('#biography > p').html(data.biography);
-});
-
-$($('#requestFriend').parent()[0]).on('ajax:success', function (e, data) {
-  var column = this.parentElement;
-  $(column).empty();
-  var ele = document.createElement('p');
-  ele.innerHTML = data.message;
-  column.appendChild(ele);
-});
-
-$($('#acceptFriend').parent()[0]).on('ajax:success', function (e, data) {
-  var container = this.parentElement;
-  $(container).empty();
-  var ele = document.createElement('p');
-  ele.innerHTML = data.message;
-  container.appendChild(ele);
-  // %%TODO%% Render partial instead!!
-  var friend = createFriend(data.user, data.profile);
-  $('#friends').append(friend);
-});
-
-$($('#rejectFriend').parent()[0]).on('ajax:success', function (e, data) {
-  var container = this.parentElement;
-  $(container).empty();
-  var ele = document.createElement('p');
-  ele.innerHTML = data.message;
-  container.appendChild(ele);
-});
-
-function createFriend (user, profile) {
-  var friend = document.createElement('div');
-  friend.className = 'friend'
-  var pic = document.createElement('div');
-  pic.className = "avatar thumb";
-  pic.style = "background-image: url(" + profile.avatar.thumb.url + ");";
-  friend.appendChild(pic);
-  var p = document.createElement('p');
-  p.innerHTML = user.username;
-  friend.appendChild(p);
-  return friend
-}
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,10 +4,10 @@ class ProfilesController < ApplicationController
   before_action :find_profile, only: [:show]
 
   def show
-    @friend_requests = my_profile ? current_user.friend_requests : []
-    @friends = @profile.user.friends
-    @upcoming_expeditions = @profile.user.attending_expeditions
-    @past_expeditions = @profile.user.attended_expeditions
+    @friend_requests = my_profile ? current_user.friend_requests.paginate(page: params[:page], per_page: 10) : []
+    @friends = @profile.user.friends.paginate(page: params[:page], per_page: 9)
+    @upcoming_expeditions = @profile.user.attending_expeditions.paginate(page: params[:page], per_page: 10)
+    @past_expeditions = @profile.user.attended_expeditions.paginate(page: params[:page], per_page: 10)
   end
 
   def update
